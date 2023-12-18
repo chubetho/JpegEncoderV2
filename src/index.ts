c6c1d7f7a993a1289a0cf9c5d268c82783903ad8
phatnguyen <phatnguyen@gmx.de>
2023-12-18 23:23:52 +0100

    cp

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,41 @@
-import { argv, exit } from 'node:process'
-import type { TaskResult } from 'tinybench'
-import { Bench } from 'tinybench'
-import { printTable } from 'console-table-printer'
-import { sleepSync } from 'bun'
+import { argv } from 'node:process'
 import { readPpm } from './ppm'
 import { aan, dct, sep } from './transform'
 
-const [file, _iters, _sleep] = argv.splice(2)
+const [file, _time, ..._] = argv.splice(2)
+const time = Number.isNaN(Number.parseInt(_time)) ? 10_000 : Number.parseInt(_time)
+const fns = [
+  { name: 'aan', fn: aan },
+  { name: 'sep', fn: sep },
+  { name: 'dct', fn: dct },
+]
 
-const iters = Number.parseInt(_iters)
-const sleep = Number.parseInt(_sleep)
+for (const x of fns)
+  await fn(x)
 
-const bench = new Bench({ time: 0, iterations: Number.isNaN(iters) ? 1 : iters })
-bench
-  .add('dct', () => {
-    const img = readPpm(file)
-    img.blocks.forEach((b) => {
-      dct(b.Y)
-      dct(b.Cb)
-      dct(b.Cr)
-    })
-    sleepSync(Number.isNaN(sleep) ? 0 : sleep)
-  })
-  .add('sep', () => {
-    const img = readPpm(file)
-    img.blocks.forEach((b) => {
-      sep(b.Y)
-      sep(b.Cb)
-      sep(b.Cr)
-    })
-    sleepSync(Number.isNaN(sleep) ? 0 : sleep)
-  })
-  .add('aan', () => {
-    const img = readPpm(file)
-    img.blocks.forEach((b) => {
-      aan(b.Y)
-      aan(b.Cb)
-      aan(b.Cr)
-    })
-    sleepSync(Number.isNaN(sleep) ? 0 : sleep)
-  })
+function fn(args: typeof fns[0]) {
+  return new Promise((resolve) => {
+    let count = 0
+    let stop = false
+    const id = setInterval(() => {
+      if (stop) {
+        clearInterval(id)
+        console.log(args.name, Math.round(time / count) / 1_000, 'ms')
+        resolve(undefined)
+      }
+      else {
+        count++
+        const img = readPpm(file)
+        img.blocks.forEach((b) => {
+          args.fn(b.Y)
+          args.fn(b.Cb)
+          args.fn(b.Cr)
+        })
+      }
+    }, 0)
 
-bench.run().then(() => {
-  const tables = bench.table()
-  const round = (x: number) => Math.round((x - sleep) * 1_000) / 1_000_000
-  const results = (bench.results as TaskResult[])
-    .map(({ min, max, mean, samples, totalTime, p75, p99, p995, p999 }, i) => ({
-      name: tables[i]?.['Task Name'] ?? '',
-      iters: samples.length,
-      min: round(min),
-      max: round(max),
-      mean: round(mean),
-      p75: round(p75),
-      p99: round(p99),
-      p995: round(p995),
-      p999: round(p999),
-      total: round(totalTime),
-    }))
-  printTable(results)
-})
+    setTimeout(() => {
+      stop = true
+    }, time)
+  })
+}

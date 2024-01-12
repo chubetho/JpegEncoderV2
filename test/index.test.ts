4ad3d398b033a6d4629ed99b803f14eb51dc529a
phatnguyen <phatnguyen@gmx.de>
2024-01-12 16:27:13 +0100

    cp

--- test/index.test.ts
+++ test/index.test.ts
@@ -25,23 +25,3 @@ it('medium', () => {
   expect(buffer).toMatchSnapshot()
   writeFileSync('src/output/medium.jpg', buffer)
 })
-
-it.skip('big_sub', () => {
-  const buffer = encoder('src/assets/big.ppm', { subsampling: true })
-  writeFileSync('src/output/big_sub.jpg', buffer)
-})
-
-it.skip('big', () => {
-  const buffer = encoder('src/assets/big.ppm', { subsampling: false })
-  writeFileSync('src/output/big.jpg', buffer)
-})
-
-it.skip('nasa_sub', () => {
-  const buffer = encoder('src/assets/nasa.ppm', { subsampling: true })
-  writeFileSync('src/output/nasa_sub.jpg', buffer)
-})
-
-it.skip('nasa', () => {
-  const buffer = encoder('src/assets/nasa.ppm', { subsampling: false })
-  writeFileSync('src/output/nasa.jpg', buffer)
-})
